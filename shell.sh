bc92cfd65faffcde171687951deb34540a01fe7c
woojjn <rhdnwls1997@gmail.com>
2022-03-22 09:07:49 +0000

    add shell.sh

---
+++ shell.sh
@@ -0,0 +1,6 @@
+
+python train.py --model abc --seed 42 --best_save_dir ./best1
+python train.py --model abc --seed 43 --best_save_dir ./best2
+python train.py --model abc --seed 44 --best_save_dir ./best3
+python train.py --model abc --seed 45 --best_save_dir ./best4
+python train.py --model abc --seed 46 --best_save_dir ./best5
