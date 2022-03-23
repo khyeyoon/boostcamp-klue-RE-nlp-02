4a06c34ccbaca2b43f70a3154a737a220396f3d6
Hyeyoon Kang <48987027+khyeyoon@users.noreply.github.com>
2022-03-23 17:49:11 +0900

    Delete shell.sh

--- shell.sh
+++
@@ -1,6 +0,0 @@
-
-python train.py --model skt/kogpt2-base-v2 --seed 10 --best_save_dir ./best1
-python train.py --model skt/kogpt2-base-v2 --seed 20 --best_save_dir ./best2
-python train.py --model skt/kogpt2-base-v2 --seed 30 --best_save_dir ./best3
-python train.py --model skt/kogpt2-base-v2 --seed 40 --best_save_dir ./best4
-python train.py --model skt/kogpt2-base-v2 --seed 50 --best_save_dir ./best5
