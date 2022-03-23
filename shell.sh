d1780544181b9f43dc12e075b6a896d3d40fb35f
woojjn <76618935+woojjn@users.noreply.github.com>
2022-03-23 11:07:59 +0900

    Delete shell.sh

--- shell.sh
+++
@@ -1,6 +0,0 @@
-
-python train.py --model monologg/koelectra-base-v3-discriminator --seed 42 --best_save_dir ./best1
-python train.py --model monologg/koelectra-base-v3-discriminator --seed 43 --best_save_dir ./best2
-python train.py --model monologg/koelectra-base-v3-discriminator --seed 44 --best_save_dir ./best3
-python train.py --model monologg/koelectra-base-v3-discriminator --seed 45 --best_save_dir ./best4
-python train.py --model monologg/koelectra-base-v3-discriminator --seed 46 --best_save_dir ./best5
